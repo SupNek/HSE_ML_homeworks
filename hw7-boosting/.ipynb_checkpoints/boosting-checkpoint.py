9ac2634e086d1147418a742509eaf8d6440aecd4
Nikita Zhigalov <124699797+SupNek@users.noreply.github.com>
2025-04-18 22:05:17 +0300

    added all notebooks

---
+++ hw7-boosting/.ipynb_checkpoints/boosting-checkpoint.py
@@ -0,0 +1,107 @@
+from __future__ import annotations
+
+from collections import defaultdict
+
+import numpy as np
+from sklearn.metrics import roc_auc_score
+from sklearn.tree import DecisionTreeRegressor
+
+
+def score(clf, x, y):
+    return roc_auc_score(y == 1, clf.predict_proba(x)[:, 1])
+
+
+class Boosting:
+
+    def __init__(
+            self,
+            base_model_params: dict = None,
+            n_estimators: int = 10,
+            learning_rate: float = 0.1,
+            subsample: float = 0.3,
+            early_stopping_rounds: int = None,
+            plot: bool = False,
+    ):
+        self.base_model_class = DecisionTreeRegressor
+        self.base_model_params: dict = {} if base_model_params is None else base_model_params
+
+        self.n_estimators: int = n_estimators
+
+        self.models: list = []
+        self.gammas: list = []
+
+        self.learning_rate: float = learning_rate
+        self.subsample: float = subsample
+
+        self.early_stopping_rounds: int = early_stopping_rounds
+        if early_stopping_rounds is not None:
+            self.validation_loss = np.full(self.early_stopping_rounds, np.inf)
+
+        self.plot: bool = plot
+
+        self.history = defaultdict(list)
+
+        self.sigmoid = lambda x: 1 / (1 + np.exp(-x))
+        self.loss_fn = lambda y, z: -np.log(self.sigmoid(y * z)).mean()
+        self.loss_derivative = lambda y, z: -y * self.sigmoid(-y * z)
+
+    def fit_new_base_model(self, x, y, predictions):
+        model = self.base_model_class(**self.base_model_params) # создаем базовую модель
+        new_indexes = np.random.choice(np.arange(x.shape[0]), size=int(x.shape[0] * self.subsample)) # создаем выборку бутстрапом
+        model.fit(x[new_indexes], y[new_indexes])
+
+        new_gamma = self.find_optimal_gamma(y[new_indexes], 
+                                            predictions[new_indexes],   
+                                            model.predict(x[new_indexes]))
+        self.gammas.append(new_gamma)
+        self.models.append(model)
+
+    def fit(self, x_train, y_train, x_valid, y_valid):
+        """
+        :param x_train: features array (train set)
+        :param y_train: targets array (train set)
+        :param x_valid: features array (validation set)
+        :param y_valid: targets array (validation set)
+        """
+        train_predictions = np.zeros(y_train.shape[0])
+        valid_predictions = np.zeros(y_valid.shape[0])
+        loss_history = []
+        best_loss = self.loss_fn(valid_predictions, y_valid)
+        count = 0
+        for _ in range(self.n_estimators):
+            self.fit_new_base_model(x_train, y_train, train_predictions)
+            train_predictions += self.learning_rate * self.models[-1].predict(x_train)
+            valid_predictions += self.learning_rate * self.models[-1].predict(x_valid)
+            loss_history += [self.loss_fn(valid_predictions, y_valid)]
+            if self.early_stopping_rounds is not None:
+                if loss_history[-1] >= best_loss:
+                    count += 1
+                    if count >= self.early_stopping_rounds:
+                        break
+                else:
+                    count = 0
+                    best_loss = loss_history[-1]
+                
+        if self.plot:
+            plt.plot(np.arange(len(loss_history)), loss_history)
+            
+    def predict_proba(self, x):
+        prediction = np.zeros(x.shape[0])
+        for gamma, model in zip(self.gammas, self.models):
+            prediction += model.predict(x) * gamma
+        return np.array([(1 - self.sigmoid(i), self.sigmoid(i)) for i in prediction])
+    def find_optimal_gamma(self, y, old_predictions, new_predictions) -> float:
+        gammas = np.linspace(start=0, stop=1, num=100)
+        losses = [self.loss_fn(y, old_predictions + gamma * new_predictions) for gamma in gammas]
+
+        return gammas[np.argmin(losses)]
+
+    def score(self, x, y):
+        return score(self, x, y)
+
+    @property
+    def feature_importances_(self):
+        importances = np.zeros()
+        for model in self.models:
+            importances
+            
